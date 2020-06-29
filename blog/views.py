d5ffc1c845f823c1b5e74d767511de45faedc6cf
seokjae hong <hsj2334@algorithmlabs.co.kr>
2020-06-29 17:20:34 +0900

    edit view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from blog.forms import PostForm
 from blog.models import Post
 
 
@@ -13,3 +14,19 @@ def post_detail(request, pk):
     # post = get_object_or_404(Post, pk)
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', context={"post": post})
+
+
+def post_edit(request, pk):
+    # post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk)
+    # print(request.method)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
